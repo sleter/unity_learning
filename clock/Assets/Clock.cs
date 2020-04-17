1a8c4dd15c829f9f6fef29656bdea6c221c17252
sleter <sleter97@gmail.com>
2020-04-17 14:26:04 +0200

    init

---
+++ clock/Assets/Clock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+    const float degreesPerHour = 30f;
+    const float degreesPerMinute = 6f;
+    const float degreesPerSecond = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime dt = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, dt.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, dt.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, dt.Second * degreesPerSecond, 0f);
+    }
+}
